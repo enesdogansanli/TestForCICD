2cbed0776b501ccebb50eac6d395d12f43e2c22c
Enes Doğan <enesdogansanli@gmail.com>
2023-03-15 13:55:39 +0300

    Update hello world app

---
+++ hello_world/main/hello_world_main.c
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include <stdio.h>
+
+void app_main(void) {
+
+  while (true) {
+    printf("Hello world!\n");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
